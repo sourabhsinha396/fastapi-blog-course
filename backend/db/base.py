db5e27ec56c1dfb80f390248795fbef4ab8fc0ad
sourabh27 <sourabh@chat360.io>
2023-07-20 16:40:21 +0530

    creating tables with alembic

--- backend/db/base.py
+++ backend/db/base.py
@@ -1,1 +1,4 @@
 from db.base_class import Base
+from db.models.user import User
+from db.models.blog import Blog
+
--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
 from core.config import settings
-from db.session import engine
-from db.base_class import Base
-
-def create_tables():
-    Base.metadata.create_all(bind=engine)
 
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
-    create_tables()
     return app 
 
 
