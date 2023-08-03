aceea98ac02aa6a693259489960a8d34bcd045be
sourabh27 <sourabh@chat360.io>
2023-08-03 12:50:22 +0530

    filter response by response model

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -5,3 +5,11 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str = Field(...,min_length=4)
     
+
+class ShowUser(BaseModel):
+    id: int 
+    email: EmailStr
+    is_active: bool 
+
+    class Config():
+        orm_mode = True
