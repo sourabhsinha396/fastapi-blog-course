1134362c7c42de7e626e6d4f6e3ef49eec2614d2
sourabh27 <sourabh@chat360.io>
2023-08-04 16:03:53 +0530

    update blog by id

--- backend/schemas/blog.py
+++ backend/schemas/blog.py
@@ -14,6 +14,8 @@ class CreateBlog(BaseModel):
             values["slug"] = values.get("title").replace(" ","-").lower()
         return values
     
+class UpdateBlog(CreateBlog):
+    pass
 
 class ShowBlog(BaseModel):
     title: str 
