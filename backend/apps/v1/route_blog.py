e198eda003d2004434cfa690c10900d9dce71869
sourabh27 <sourabh@chat360.io>
2023-09-02 16:06:19 +0530

    blogs list view

--- backend/apps/v1/route_blog.py
+++ backend/apps/v1/route_blog.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
-from fastapi import Request
+from fastapi import Request, Depends
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session 
+from db.repository.blog import list_blogs
+from db.session import get_db
 
 
 templates = Jinja2Templates(directory="templates")
@@ -8,5 +11,7 @@ router = APIRouter()
 
 
 @router.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("blogs/home.html", {"request": request})
+def home(request: Request, db: Session = Depends(get_db)):
+    blogs = list_blogs(db=db)
+    context = {"request": request,"blogs":blogs}
+    return templates.TemplateResponse("blogs/home.html", context=context)
