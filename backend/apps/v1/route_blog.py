42b09d2b1a22b586b2d17af7c885892eb95100b0
sourabh27 <sourabh@chat360.io>
2023-09-02 21:27:54 +0530

    blog detailview

--- backend/apps/v1/route_blog.py
+++ backend/apps/v1/route_blog.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from fastapi import Request, Depends
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session 
-from db.repository.blog import list_blogs
+from db.repository.blog import list_blogs, retreive_blog
 from db.session import get_db
 
 
@@ -15,3 +15,9 @@ def home(request: Request, db: Session = Depends(get_db)):
     blogs = list_blogs(db=db)
     context = {"request": request,"blogs":blogs}
     return templates.TemplateResponse("blogs/home.html", context=context)
+
+
+@router.get("/app/blog/{id}/")
+def blog_detail(request: Request, id: int, db: Session = Depends(get_db)):
+    blog = retreive_blog(id=id, db=db)
+    return templates.TemplateResponse("blogs/detail.html",{"request":request,"blog":blog})
